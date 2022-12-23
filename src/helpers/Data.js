5b5c7925c97dd857c168116f7c1127664a6df244
Jhony A Saenz <jash0310@gmail.com>
2022-12-23 14:37:44 -0500

    actualizando skills y actualizando responsive

---
+++ src/helpers/Data.js
@@ -0,0 +1,89 @@
+import angular from "../assets/skills/angular.png"
+import bootstrap from "../assets/skills/bootstrap.png"
+import css from "../assets/skills/css.png"
+import html from "../assets/skills/html.png"
+import javascript from "../assets/skills/javascript.png"
+import nodejs from "../assets/skills/nodejs.png"
+import postgresql from "../assets/skills/postgresql.png"
+import react from "../assets/skills/react.png"
+import redux from "../assets/skills/redux.png"
+import sequelize from "../assets/skills/sequelize.png"
+import typescript from "../assets/skills/typescript.png"
+import ngrx from "../assets/skills/ngrx.png"
+import express from "../assets/skills/express.png"
+import github from "../assets/skills/github.png"
+
+
+export const technologies = [
+    {
+        title: 'React',
+        image: react,
+        link: 'https://reactjs.org/'
+    },
+    {
+        title: 'Redux',
+        image: redux,
+        link: 'https://redux.js.org/'
+    },
+    {
+        title: 'Angular',
+        image: angular,
+        link: 'https://angular.io/'
+    },
+    {
+        title: 'NgRx',
+        image: ngrx,
+        link: 'https://ngrx.io/'
+    },
+    {
+        title: 'HTML',
+        image: html,
+        link: 'https://developer.mozilla.org/es/docs/Web/HTML'
+    },
+    {
+        title: 'CSS',
+        image: css,
+        link: 'https://developer.mozilla.org/es/docs/Web/CSS'
+    },
+    {
+        title: 'Bootstrap',
+        image: bootstrap,
+        link: 'https://getbootstrap.com/'
+    },
+    {
+        title: 'JavaScript',
+        image: javascript,
+        link: 'https://www.javascript.com'
+    },
+    {
+        title: 'NodeJS',
+        image: nodejs,
+        link: 'https://nodejs.org/'
+    },
+    {
+        title: 'Express',
+        image: express,
+        link: 'https://expressjs.com/'
+    },
+    {
+        title: 'TypeScript',
+        image: typescript,
+        link: 'https://www.typescriptlang.org/'
+    },
+    {
+        title: 'Sequelize',
+        image: sequelize,
+        link: 'https://sequelize.org/'
+    },
+    {
+        title: 'PostgreSQL',
+        image: postgresql,
+        link: 'https://www.postgresql.org/'
+    },
+    {
+        title: 'GitHub',
+        image: github,
+        link: 'https://github.com/'
+    },
+]
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import EmailIcon from "@material-ui/icons/Email";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import React from 'react';
 import "../styles/Home.css"
+import { technologies } from '../helpers/Data'
 
 function Home() {
     return (
@@ -11,7 +12,9 @@ function Home() {
                 <h2>Hola, mi nombre Jhony Alberto Saenz Hurtado</h2>
                 <div className='prompt'>
                     <p>
-                        Soy Programador Full Stack Web Developer y Estudiante de Ingenieria de Software
+                        Soy Full Stack Web Develop, Tecnólogo en sistemas de información, estudiante de Ingeniería de Sistemas entre otros cursos que he obtenido a lo largo de mi vida profesional,
+                        actualmente estoy trabajando para NCR que es el líder mundial en tecnología empresarial para restaurantes, minoristas y bancos.
+                        Mi vida profesional siempre ha estado enfocada al ámbito de la tecnología, pero más para el lado del servicio.
                     </p>
                     <a href="https://www.linkedin.com/in/jhony-saenz-hurtado/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
                     <a href="https://github.com/jasaenzh" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
@@ -20,22 +23,15 @@ function Home() {
             </div>
             <div className='skills'>
                 <h1>Skills</h1>
-                <ol className="list">
-                    <li className="item">
-                        <h2>Front-End</h2>
-                        <span>
-                            React, Angular, Redux, HTML, CSS, BootStrap
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2>Back-End</h2>
-                        <span> Node.js, Express, Sequelize, Postgres </span>
-                    </li>
-                    <li className="item">
-                        <h2>Languages</h2>
-                        <span>JavaScript, TypeScript</span>
-                    </li>
-                </ol>
+                <div className="photo-gallery">
+                    {technologies.map(data => (
+                        <div className="photo-container">
+                            <a target='_blank' href={data.link} rel="noreferrer"><img className='skills_icons' src={data.image} alt="imagenSkill" /></a>
+                            <p className='card-text'>{data.title}</p>
+                        </div>
+                    ))}
+
+                </div>
             </div>
         </div>
     )
